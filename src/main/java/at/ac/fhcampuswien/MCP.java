c02c529d4d36ef73aa71c8495f2aeac4758bf45e
merve goekce <merve.goekce@stud.fh-campuswien.ac.at>
2021-07-01 21:07:23 +0200

    Exercise_4_Thread

--- src/main/java/at/ac/fhcampuswien/MCP.java
+++ src/main/java/at/ac/fhcampuswien/MCP.java
@@ -5,6 +5,6 @@ import at.ac.fhcampuswien.newsanalyzer.ui.UserInterface;
 public class MCP {
     public static void main(String[] args) {
         UserInterface ui = new UserInterface();
-        ui.start();
+        ui.start() ;
     }
 }
